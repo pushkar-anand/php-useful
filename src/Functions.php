de3f47ac877cb4adade743eb0e9ba02874237c73
Pushkar Anand <anandpushkar088@gmail.com>
2018-06-08 12:46:17 +0530

    two basic functions ready.

---
+++ src/Functions.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Functions;
+
+
+class Functions
+{
+    /**
+     * Returns a randomly generated string for given length
+     * @param int $length
+     * @return string
+     */
+    static function random_string(int $length): string
+    {
+        $characters = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789';
+        $string = '';
+        for ($i = 0; $i < $length; $i++) {
+            $string .= $characters[rand(0, strlen($characters) - 1)];
+        }
+        return $string;
+    }
+
+    /**
+     * escapes a given string
+     * @param string $input
+     * @return string
+     */
+    static function escape_input(string $input): string
+    {
+        $escaped_input = htmlspecialchars(stripslashes(trim($input)));
+        return $escaped_input;
+    }
+
+
+}
