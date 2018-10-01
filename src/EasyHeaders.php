1d1de623ebd1750dca64ba444593e82d8d9da145
aravindgopall <aravindgopal.ag@gmail.com>
2018-10-01 20:35:57 +0530

    Update EasyHeaders.php
    added generic header builders and also few more http headers mostly useful

--- src/EasyHeaders.php
+++ src/EasyHeaders.php
@@ -10,35 +10,180 @@ namespace PhpUseful;
  */
 class EasyHeaders
 {
+    static function moved_permanently()
+    {
+        header($_SERVER['SERVER_PROTOCOL'] . ' 301 MOVED PERMANENTLY',
+            true,
+            301);
+        exit();
+    }
+
+    static function found()
+    {
+        header($_SERVER['SERVER_PROTOCOL'] . ' 302 FOUND',
+            true,
+            302);
+        exit();
+    }
+
     static function bad_request()
     {
         header($_SERVER['SERVER_PROTOCOL'] . ' 400 BAD REQUEST',
             true,
             400);
         exit();
-
     }
 
-    static function server_error()
+    static function unauthorized()
     {
-        header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error',
+        header($_SERVER['SERVER_PROTOCOL'] . ' 401 UNAUTHORIZED',
             true,
-            500);
+            401);
         exit();
+    }
 
+    static function forbidden()
+    {
+        header($_SERVER['SERVER_PROTOCOL'] . ' 403 FORBIDDEN',
+            true,
+            403);
+        exit();
     }
 
     static function not_found()
     {
-        header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found',
+        header($_SERVER['SERVER_PROTOCOL'] . ' 404 NOT FOUND',
             true,
             404);
+        exit();
+    }
+
+    static function request_timeout()
+    {
+        header($_SERVER['SERVER_PROTOCOL'] . ' 408 REQUEST TIMEOUT',
+            true,
+            408);
+        exit();
+    }
+
+    static function server_error()
+    {
+        header($_SERVER['SERVER_PROTOCOL'] . ' 500 INTERNAL SERVER ERROR',
+            true,
+            500);
+        exit();
+    }
+
+    static function not_implemented()
+    {
+        header($_SERVER['SERVER_PROTOCOL'] . ' 501 NOT IMPLEMENTED',
+            true,
+            501);
+        exit();
+    }
+
+    static function bad_gateway()
+    {
+        header($_SERVER['SERVER_PROTOCOL'] . ' 502 BAD GATEWAY',
+            true,
+            502);
+        exit();
+    }
+
+    static function service_unavailable()
+    {
+        header($_SERVER['SERVER_PROTOCOL'] . ' 503 SERVICE UNAVAILABLE',
+            true,
+            503);
+        exit();
+    }
+
+    static function gateway_timeout()
+    {
+        header($_SERVER['SERVER_PROTOCOL'] . ' 504 GATEWAY TIMEOUT',
+            true,
+            504);
         exit();
     }
 
     static function json_header()
     {
         header('Content-Type: application/json');
+    }
+
+    static function pdf_header()
+    {
+        header('Content-Type: application/pdf');
+    }
+
+    static function xml_header()
+    {
+        header('Content-Type: application/xml');
+    }
+
+    static function download_header()
+    {
+        header('Content-Type: application/force-download');
+    }
+
+    static function html_header()
+    {
+        header('Content-Type: text/html');
+    }
+
+    static function css_header()
+    {
+        header('Content-Type: text/css');
+    }
+
+    static function js_header()
+    {
+        header('Content-Type: text/javascript');
+    }
+
+    static function text_header()
+    {
+        header('Content-Type: text/plain');
+    }
+    
+    static function base_header(string $userName, string $password)
+    {
+        header('Authorization : Basic '. base64_encode($userName.$password));
+    }
+    
+    static function urlEncoded_header()
+    {
+        header('Content-Type : application/x-www-form-urlencoded');
+    }
+    
+    static function form_header()
+    {
+        header('Content-Type : multipart/form-data');
+    }
+    
+    static function origin_header(string $origin)
+    {
+        header('origin : '.$origin);
+    }
+    
+    static function set_cookie_header(string $cookie)
+    {
+        header('Set-Cookie : '.$cookie);
+    }
+    
+    static function allow_cross_origin_header(string $paths)
+    {
+        header('Access-Control-Allow-Origin: '.$paths);
+    }
+    
+    static function build_any_header(string $header)
+    {
+        header($header);
+    }
+    
+    static function build_any_header(string $type, string $val)
+    {
+        header($type." : ".$val);
     }
 
 }
