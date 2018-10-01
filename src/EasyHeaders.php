a6c3443fd89dd4db9e4ab2a59a49252ce9efdb65
Toni Peric <thetoniperic@gmail.com>
2018-10-01 16:36:10 +0200

    Add common headers. Closes pushkar-anand/php-useful#2

--- src/EasyHeaders.php
+++ src/EasyHeaders.php
@@ -36,6 +36,30 @@ class EasyHeaders
         exit();
     }
 
+    static function forbidden()
+    {
+        header($_SERVER['SERVER_PROTOCOL'] . ' 403 Forbidden',
+            true,
+            403);
+        exit();
+    }
+
+    static function unauthorized()
+    {
+        header($_SERVER['SERVER_PROTOCOL'] . ' 401 Unauthorized',
+            true,
+            401);
+        exit();
+    }
+
+    static function unprocessable_entity()
+    {
+        header($_SERVER['SERVER_PROTOCOL'] . ' 422 Unprocessable Entity',
+            true,
+            422);
+        exit();
+    }
+
     static function json_header()
     {
         header('Content-Type: application/json');
